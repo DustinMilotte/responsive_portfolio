4c118a0a66b85f19009a15b971db4583fa933400
Dustin Milotte <dmilotte@gmail.com>
2019-01-25 12:27:34 -0500

    begin home work, add slider

--- dist/js/main.js
+++ dist/js/main.js
@@ -57,6 +57,13 @@ function scrollPage() {
   
 }
 
-function scrollToTop() {
 
-}
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 3,
+  dots: '#dots',
+  draggable: true,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
